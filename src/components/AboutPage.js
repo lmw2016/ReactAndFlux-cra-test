544f7934fe0971787b4236d7d532d159541753a9
liming wu <limwuly@gmail.com>
2019-09-16 20:44:55 -0400

    create 1st class component

---
+++ src/components/AboutPage.js
@@ -0,0 +1,15 @@
+import React from "react"; //lower case R
+
+
+class AboutPage extends React.Component{
+    render(){
+        return (
+            <>
+            <h1>About</h1>
+            <p>This app use react</p>
+            </>
+        );
+    }
+}
+
+export default AboutPage;
--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import React from "react"
 import {render} from "react-dom";
 import HomePage from "./components/HomePage"
+import AboutPage from "./components/AboutPage"
 
-render(<HomePage/>, document.getElementById("root"))
+// render(<HomePage/>, document.getElementById("root"))
+render(<AboutPage />, document.getElementById("root"));
