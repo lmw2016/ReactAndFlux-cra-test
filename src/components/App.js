575dbf5ad2a5841ae014c629e13ce107fabe39e9
liming wu <limwuly@gmail.com>
2019-09-29 19:55:02 -0400

    implement Course page list using state and lifecycle

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,22 @@ import React from "react";
 import HomePage from "./HomePage"; //fold path is important
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
+import CoursesPage from "./CoursesPage";
 
-function App(){
-    function getPage(){
-        const route=window.location.pathname;
-        if(route==="/about")
-        return <AboutPage />;
-        return <HomePage/>;
-    }
+function App() {
+  function getPage() {
+    const route = window.location.pathname;
+    if (route === "/courses") return <CoursesPage />;
+    if (route === "/about") return <AboutPage />;
+    return <HomePage />;
+  }
 
-    return  (
-      <div className="container-fluid">
-          <Header/>
-          {getPage()}
-      </div>
-  )
+  return (
+    <div className="container-fluid">
+      <Header />
+      {getPage()}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/CoursesPage.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { getCourses } from "../api/courseApi";
+
+class CoursesPage extends React.Component{
+  state={
+      courses:[]
+  };
+
+  componentDidMount(){ //Mount
+      getCourses().then(courses=>this.setState({courses:courses}));
+  }
+
+    render(){
+        return (<>
+        <h2>Courses</h2>
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Author ID</th>
+                    <th>Category</th>
+                </tr>
+            </thead>
+            <tbody>
+                {this.state.courses.map(course=>{
+                    return (
+                     <tr key={course.id}>
+                        <th>{course.title}</th>
+                        <th>{course.authorId}</th>
+                        <th>{course.category}</th>
+                    </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+        </>
+        )
+    }
+}
+
+export default CoursesPage;
