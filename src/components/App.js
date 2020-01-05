a5332e589b1fc7fb6aaab6d00685147dfd659024
liming wu <limwuly@gmail.com>
2020-01-04 20:44:25 -0500

    implement Router and route

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,16 @@ import HomePage from "./HomePage"; //fold path is important
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
 import CoursesPage from "./CoursesPage";
+import {Route} from "react-router-dom";
 
 function App() {
-  function getPage() {
-    const route = window.location.pathname;
-    if (route === "/courses") return <CoursesPage />;
-    if (route === "/about") return <AboutPage />;
-    return <HomePage />;
-  }
 
   return (
     <div className="container-fluid">
       <Header />
-      {getPage()}
+      <Route path="/" exact component={HomePage}/>
+      <Route path="/courses" component={CoursesPage}/>
+      <Route path="/about" component={AboutPage}/>
     </div>
   );
 }
