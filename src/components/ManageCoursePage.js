7e797e8eb6f6b9feb5bd069466e459fbc679df68
liming wu <limwuly@gmail.com>
2020-01-26 13:25:59 -0500

    Prompt

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+// import { Prompt } from "react-router-dom";
 
 const ManageCoursePage=props=>{
     return (
         <>
           <h2>Manage Course</h2>
+          {/* <Prompt when="true" message="Are you sure you want leave?" /> */}
           {props.match.params.slug}
         </>
     )
