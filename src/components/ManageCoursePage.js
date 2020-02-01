53c0fe1d672a1bd19e2b0c73c4d0e63cc35397dd
liming wu <limwuly@gmail.com>
2020-02-01 14:27:48 -0500

    implement course form

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React,{useState} from 'react';
+import CourseForm from './common/CourseForm'
+import CoursesPage from './CoursesPage';
 // import { Prompt } from "react-router-dom";
 
 const ManageCoursePage=props=>{
+
+   const [course,setCourse]=useState({ //array destructing
+     Id:null,
+     slug:"",
+     title:"",
+     authorId:null,
+     category:""
+   })
+
+  //  function handleTitleChange(event){
+  //    const updatedCourse={...course,title:event.target.value};
+  //    setCourse(updatedCourse);
+  //  }
+
+   function handleChange({target}){
+    const updatedCourse={...course, 
+      [target.name]:target.value};
+    setCourse(updatedCourse);
+  }
+
     return (
         <>
           <h2>Manage Course</h2>
           {/* <Prompt when="true" message="Are you sure you want leave?" /> */}
-          {props.match.params.slug}
+          {/* {props.match.params.slug} */}
+          {/* <CourseForm course={course} onTitleChange={handleTitleChange} /> */}
+
+          <CourseForm course={course} onChange={handleChange} />
         </>
     )
 }
