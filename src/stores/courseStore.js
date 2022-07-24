8526b5f70e5926ff31a20a05cb1f66f49c8f5a22
liming wu <limwuly@gmail.com>
2022-07-24 17:00:13 -0400

    stores registration and private storage

--- src/stores/courseStore.js
+++ src/stores/courseStore.js
@@ -1,6 +1,10 @@
 import { EventEmitter } from "events";
+import dispatcher, { Dispatcher } from "../appDispatcher";
+import actionType from '../actions/actionTypes';
+import actionTypes from "../actions/actionTypes";
 
 const CHANGE_EVENT = "change";
+let _courses = [];
 
 class CourseStore extends EventEmitter {
     addChangeListener(callback) {
@@ -14,7 +18,26 @@ class CourseStore extends EventEmitter {
     emitChange() {
         this.emit(CHANGE_EVENT);
     }
+
+    getCourses() {
+        return _courses;
+    }
+
+    getCourseBySlug(Slug) {
+        return _courses.find(course => course.slug === Slug);
+    }
 }
 
 const store = new CourseStore();
+
+dispatcher.register(action => {
+    switch (action.actionType) {
+        case actionTypes.CREATE_COURSE:
+            _courses.push(action.course);
+            store.emitChange();
+            break;
+        default:
+            //nothing to do
+    }
+})
 export default store;
