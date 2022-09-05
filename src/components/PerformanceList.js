c3d17e1a8eb36a1f35280985cc57cae8d53edbdd
liming wu <limwuly@gmail.com>
2022-09-05 15:43:36 -0400

    implement performances paging

--- src/components/PerformanceList.js
+++ src/components/PerformanceList.js
@@ -1,4 +1,5 @@
-import React from "react"
+import React from "react";
+import {Link} from "react-router-dom"
 
 function PerformanceList(props) {
     return (
@@ -7,16 +8,18 @@ function PerformanceList(props) {
                 <tr>
                     <th>&nbsp;</th>
                     <th>Fly date</th>
+                    <th>Fly Number</th>
                     <th>Mkt Carrier</th>
                     <th>Origin City</th>
-            <th>Dest</th>
+            <th>Dest City</th>
         </tr>
     </thead>
     <tbody>
-                {props.performances.map((perf, index) => {
+                {props.currentPerformances.map((perf, index) => {
                     return (<tr key={index+1}>
-                        <th>{index+1}</th>
+                        <th>{index + 1 + props.currentPage * props.perPage}</th>
                         <th>{ new Date(perf.FlyDate).toLocaleString()}</th>
+                        <th><Link to={"/performance/"+perf.MktCarrierFlyNum}>{ perf.MktCarrierFlyNum}</Link></th>
                         <th>{perf.MktCarrier}</th>
                         <th>{ perf.OriginCity}</th>
                         <th>{ perf.DestCityName}</th>
--- src/components/PerformancesPage.js
+++ src/components/PerformancesPage.js
@@ -1,18 +1,56 @@
 import React, { useEffect, useState } from "react";
 import { getPerformances } from "../api/performanceApi";
 import PerformanceList from "./PerformanceList";
+import ReactPaginate from 'react-paginate';
+import "./App.css";
+
+
+const PER_PAGE = 11;
 
 function PerformancesPage() {
     const [performances, setPerformances] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0);
 
     useEffect(() => {
-        getPerformances().then(_performances=>setPerformances(_performances));
-        
-    },[])
+        getPerformances().then(_performances => setPerformances(_performances));
+    }, []);
+
+    function handlePageClick({ selected: selectedPage }) {
+        setCurrentPage(selectedPage);
+    }
+
+    const offset = currentPage * PER_PAGE;
+
+    const currentPagePerformances = performances
+        .slice(offset, offset + PER_PAGE);
+        // .map((perf, index) => {
+        //     return (<tr key={index + 1}>
+        //         <th>{index + 1} &nbsp;</th>
+        //         <th>{new Date(perf.FlyDate).toLocaleString()}</th>
+        //         <th>{perf.MktCarrier}</th>
+        //         <th>{perf.OriginCity}</th>
+        //         <th>{perf.DestCityName}</th>
+        //     </tr>)
+        // });
+    
+    const pageCount = Math.ceil(performances.length / PER_PAGE);
     
     return (<>
-        <h3>Performances</h3>
-        <PerformanceList performances={performances} />
+        <h3>Performances (total {performances.length}):</h3>
+        <PerformanceList currentPerformances={currentPagePerformances}
+            perPage={PER_PAGE} currentPage={currentPage} /> 
+    
+        <ReactPaginate
+            previousLabel={"<- Previous"}
+            nextLabel={"Next ->"}
+            pageCount={pageCount}
+            onPageChange={handlePageClick}
+            containerClassName={"pagination"}
+            previousLinkClassName={"pagination__link"}
+            nextLinkClassName={"pagination__link"}
+            disabledClassName={"pagination__link--disabled"}
+            activeClassName={"pagination__link--active"}
+        />
         </>
     )
 }
